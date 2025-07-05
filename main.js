995f877b9a337b8dd8edf9a851a6655807f091e8
Muhammadkul-Khurboyev <154499804+Muhammadkul-Khurboyev@users.noreply.github.com>
2025-07-05 20:25:45 +0500

    Transaction page added

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const elSiteNavbar = document.querySelector('.site-header_navbar');
 const elCloseBtn = document.querySelector('.close-btn');
 const elRoadListItem = document.querySelectorAll('.road-list_item');
 const elNavbarListItem = document.querySelectorAll('.navbar-list_item');
+const elHeroSection = document.querySelector('.hero-section_end');
+const elTransactionSection = document.querySelector('.transaction');
+// const elHeroSectionOpen = document.querySelector('.open-hero');
 
 
 elHamburgBtn.addEventListener('click', ()=> {
@@ -19,7 +22,18 @@ elCloseBtn.addEventListener('click', ()=> {
 elRoadListItem.forEach(item => {
 	item.addEventListener('click', ()=> {
 		elRoadListItem.forEach(el => el.classList.remove('active'));
+		console.log(item.textContent);
 		item.classList.add('active');
+		if(item.textContent.trim() == 'Overview') {
+			elHeroSection.classList.remove('close-hero');
+			elTransactionSection.classList.add('close-hero');
+		}
+		else if (item.textContent.trim() == 'Transaction') {
+			elHeroSection.classList.add('close-hero');
+			elTransactionSection.classList.remove('close-hero');
+		}
+		
+		
 	})
 });
 
